79b03cff8b4c551cb2222354a3f6ac4eb19d26b2
louis <louis@host.docker.internal>
2022-03-02 18:59:39 -0800

    Added a enhanced for loop to loop through the options of games.

--- Java_Games/src/com/games/choose/ChooseAGame.java
+++ Java_Games/src/com/games/choose/ChooseAGame.java
@@ -4,24 +4,28 @@ import com.games.ScannerHelper;
 import com.games.numberguessing.*;
 
 public class ChooseAGame {
+
 	
 	public void chooseAGame(){
-	System.out.println("Please choose a game to play 1.Number Guessing.");
-	System.out.println("Please choose a game to play 2.Lottery.");
-	System.out.println("Please choose a game to play 3.Rock,Papper & Scissor. ");
-	System.out.println("Please choose a game to play 4.Space Man ");
-	System.out.println("Please choose a game to play 5.More Games to Come. ");
-	
-	int playerChoice = ScannerHelper.ReadInputInt();
-	
-	if(playerChoice == 1) {
-		System.out.println("Number Guessing Game");
-		NumberGuessingGame guess = new NumberGuessingGame();
-		guess.numberGuessingGame();
-	}else {
-		System.out.println("Please choose a game." );
 		
+		String [] gamesNames = {"1.Number Guessing","2.Lottery","3.Rock,Papper & Scissor","4.Space Man","5.More Games to Come"};
+		
+		System.out.println("Please choose a game to play:");
+		
+		for(String gameName : gamesNames) {
+		System.out.println(gameName);
+		}
+	
+		while(true) {
+		int playerChoice = ScannerHelper.ReadInputInt();
+		
+		if(playerChoice == 1) {
+			System.out.println("Number Guessing Game");
+			NumberGuessingGame guess = new NumberGuessingGame();
+			guess.numberGuessingGame();
+		}else {
+			System.out.println("Please choose a game." );
+		}
+		}
 	}
-	}
-
 }
